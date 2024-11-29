04dc26ab25575daf9e24b7988ab3a4d148c389ba
SheharyarXDツ <1510sheharyarxd@gmail.com>
2024-11-29 18:14:41 +0500

    Verceljson added

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
     port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false },
+    ssl: process.env.DB_SSL === 'true'
 });
 
 pool.connect()
